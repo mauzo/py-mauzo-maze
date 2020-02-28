6961cc756c7ac32e7a2b4a20cbfe6c0b566de285
Ben Morrow <ben@morrow.me.uk>
2020-02-28 16:39:06 +0000

    Separate out render.

---
+++ mauzo/maze/render.py
@@ -0,0 +1,71 @@
+# render.py - Render
+# These functions actually draw every frame. Most of the drawing
+# has already been done and put in the display lists.
+
+from    OpenGL.GL   import *
+
+from    .           import display
+from    .camera     import render_camera
+from    .options    import Options
+from    .player     import Player, render_player
+from    .world      import render_world
+
+# Clear the screen to remove the previous frame.
+def render_clear():
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+
+# Draw a miniview of the ground under our feet. Not very useful...
+def render_miniview ():
+    display.display_push_miniview()
+    glOrtho(-0.5, 0.5, -0.5, 0.5, 0, 1)
+
+    pos = Player["pos"]
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    glTranslatef(-pos[0], -pos[1], -pos[2])
+
+    render_world()
+
+    display.display_pop_miniview()
+
+# This is called to render every frame. We clear the window, position the
+# camera, and then call the display list to draw the world.
+def render():
+    render_clear()
+    render_camera()
+    render_world()
+    render_player()
+
+    if (Options["miniview"]):
+        render_miniview()
+        render_miniview()
+
+# Try a select buffer operation with the current view
+def render_try_select (mode):
+    glPushAttrib(GL_ENABLE_BIT)
+    if mode == "feedback":
+        glFeedbackBuffer(4096, GL_3D)
+        glRenderMode(GL_FEEDBACK)
+    else:
+        glSelectBuffer(4096)
+        glRenderMode(GL_SELECT)
+        glInitNames()
+        glPushName(1)
+    glDisable(GL_CULL_FACE)
+
+    render()
+    buf = glRenderMode(GL_RENDER)
+    glPopAttrib()
+
+    if len(buf) == 0:
+        print("No", mode, "records!")
+    for h in buf:
+        if mode == "feedback":
+            (typ, *vs) = h
+            print(typ, ",", [v.vertex[2] for v in vs])
+        else:
+            (mn, mx, nms) = h
+            print("min", mn, "max", mx, "names", nms,
+                Select[nms[0] - 1])
+
--- maze.py
+++ maze.py
@@ -14,6 +14,7 @@ from mauzo.maze.drawing     import *
 from mauzo.maze.events      import *
 from mauzo.maze.options     import *
 from mauzo.maze.player      import *
+from mauzo.maze.render      import *
 from mauzo.maze.vectors     import *
 from mauzo.maze.world       import *
 
@@ -38,70 +39,6 @@ Key_Bindings = {
     K_F3:       (["toggle", "backface"],        None),
     K_F4:       (["toggle", "miniview"],        None),
 }
-
-# Render
-# These functions actually draw every frame. Most of the drawing
-# has already been done and put in the display lists.
-
-# Clear the screen to remove the previous frame.
-def render_clear():
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-
-def render_miniview ():
-    display_push_miniview()
-    glOrtho(-0.5, 0.5, -0.5, 0.5, 0, 1)
-
-    pos = Player["pos"]
-
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
-    glTranslatef(-pos[0], -pos[1], -pos[2])
-
-    render_world()
-
-    display_pop_miniview()
-
-# This is called to render every frame. We clear the window, position the
-# camera, and then call the display list to draw the world.
-def render():
-    render_clear()
-    render_camera()
-    render_world()
-    render_player()
-
-    if (Options["miniview"]):
-        render_miniview()
-        render_miniview()
-
-# Try a select buffer operation with the current view
-def render_try_select (mode):
-    glPushAttrib(GL_ENABLE_BIT)
-    if mode == "feedback":
-        glFeedbackBuffer(4096, GL_3D)
-        glRenderMode(GL_FEEDBACK)
-    else:
-        glSelectBuffer(4096)
-        glRenderMode(GL_SELECT)
-        glInitNames()
-        glPushName(1)
-    glDisable(GL_CULL_FACE)
-
-    render()
-    buf = glRenderMode(GL_RENDER)
-    glPopAttrib()
-
-    if len(buf) == 0:
-        print("No", mode, "records!")
-    for h in buf:
-        if mode == "feedback":
-            (typ, *vs) = h
-            print(typ, ",", [v.vertex[2] for v in vs])
-        else:
-            (mn, mx, nms) = h
-            print("min", mn, "max", mx, "names", nms,
-                Select[nms[0] - 1])
-
-# Options
 
 # Events
 # These functions manage things that happen while the program is running.
